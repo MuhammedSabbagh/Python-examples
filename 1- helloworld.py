9bcc6317d993dad92e449471e5da69de3c65599e
MuhammedSabbagh <hamod.sa03@gmail.com>
2021-09-07 08:21:07 -0400

    commit #`

---
+++ 1- helloworld.py
@@ -0,0 +1,22 @@
+##this is the first piece of code every human being who know how to code have done
+
+print("hello, world! \n")
+
+
+("------------------------------------- \n")
+#here is a thing python is pretty cool when it comes to making variables
+#and you can print them easily as well
+i = 6.6565656198 + 0.546546151
+print(i)
+
+print("------------------------------------- \n")
+
+#also you can combine multiple text variables as well
+
+var1 = "my name is: "
+var2 = "muhammed"
+print(var1+var2)
+
+
+print(False)
+
